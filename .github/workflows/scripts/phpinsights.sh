331470df9bc9feabb896fe1d2465beeec387b2e2
Rafael Antonio Yon <rafaelyon.1999@gmail.com>
2022-07-20 23:43:43 -0300

    Specified flag to disable Composer plugins for workflow

--- .github/workflows/scripts/phpinsights.sh
+++ .github/workflows/scripts/phpinsights.sh
@@ -4,7 +4,7 @@
 curl https://getcomposer.org/download/2.3.10/composer.phar -o /tmp/composer.phar
 chmod +x /tmp/composer.phar
 
-/tmp/composer.phar install --no-interaction --no-progress
+/tmp/composer.phar install --no-interaction --no-progress --no-plugins
 
 # Run PHP Insights
 ./vendor/bin/phpinsights analyse --no-ansi --no-interaction --format='RafaelYon\PhpInsightsReviewer\GithubFormatter' src/
