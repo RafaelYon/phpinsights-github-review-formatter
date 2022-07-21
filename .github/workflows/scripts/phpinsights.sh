029c575264929c90c54e34bc870756c1af5942af
Rafael Antonio Yon <rafaelyon.1999@gmail.com>
2022-07-20 23:32:17 -0300

    Adicionado workflow para valida qualidade do projeto

---
+++ .github/workflows/scripts/phpinsights.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# Get dependencies
+curl https://getcomposer.org/download/2.3.10/composer.phar -o /tmp/composer.phar
+chmod +x /tmp/composer.phar
+
+/tmp/composer.phar install --no-interaction --no-progress
+
+# Run PHP Insights
+./vendor/bin/phpinsights analyse --no-ansi --no-interaction --format='RafaelYon\PhpInsightsReviewer\GithubFormatter' src/
